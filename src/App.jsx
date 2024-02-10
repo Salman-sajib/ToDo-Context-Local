5ebbbf083c6aa8fffc2d35a4ff3951613218348f
Salman-sajib <sfsajib@gmail.com>
2024-02-10 20:09:31 +0600

    added the todoContext provider to the app and set all the values, added all the functionalities needed for the todos

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,55 @@
+import { useEffect, useState } from 'react';
+
+import { TodoProvider } from './contexts';
+
+import { nanoid } from 'nanoid';
+
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (todo) => {
+    setTodos((prev) => [{ id: nanoid(), ...todo }, ...prev]);
+  };
+
+  const updateTodo = (id, todo) => {
+    setTodos((prev) =>
+      prev.map((prevTodo) => (prevTodo.id === todo.id ? todo : prevTodo))
+    );
+  };
+
+  const deleteTodo = (id) => {
+    setTodos((prev) => prev.filter((todo) => todo.id !== id));
+  };
+
+  const toggleComplete = (id) => {
+    setTodos((prev) =>
+      prev.map((prevTodo) =>
+        prevTodo.id === id
+          ? { ...prevTodo, completed: !prevTodo.completed }
+          : prevTodo
+      )
+    );
+  };
+
+  useEffect(() => {
+    const todos = JSON.parse(localStorage.getItem('todos'));
+    if (todos && todos.length > 0) {
+      setTodos(todos);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
+
   return (
-    <div>
-      <h1 className='text-3xl text-red-600'>Hello React</h1>
-    </div>
+    <TodoProvider
+      value={{ todos, addTodo, updateTodo, deleteTodo, toggleComplete }}
+    >
+      <div>
+        <h1 className='text-3xl text-red-600'>Hello React</h1>
+      </div>
+    </TodoProvider>
   );
 }
 
